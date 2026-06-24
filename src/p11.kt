a7b679116d3eefb6b9c8a9d120eb24712ab34673
chetan <chetangoswani01@gmail.com>
2026-06-24 12:32:21 +0530

    initial

---
+++ src/p11.kt
@@ -0,0 +1,49 @@
+fun main()
+{
+    var i=10
+    var j: Int
+    j=20
+    val k=200
+    j=1000
+    println("k="+k)
+    println("j="+j)
+    println("i="+i)
+
+    var m: Float
+    m=1.5f
+    println("m="+m)
+
+    var l: Boolean
+    l=false
+    println("l="+l)
+
+    var a: String
+    a="MAD"
+    println("a="+a)
+
+    var b: Double
+    b=50.22
+    println("b="+b)
+
+    var c: Char
+    c='T'
+    println("c="+c)
+
+    var d: Short
+    d=-2
+    println("d="+d)
+
+    var e: Long
+    e=36541656
+    println("e="+e)
+
+    val name="kotline"
+    println("Hello, $name!")
+    println("Sum ${5+10}")
+
+    val z=45
+    println(String.format("%d", z))
+
+    val w=42
+    println(w)
+}
---
+++ src/p12.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val i=10000
+    val j: Double =i.toDouble()
+    println(j)
+    println(i)
+
+   var a="100000"
+    var b=a.toInt()
+    var c=b.toDouble()
+
+    println("String value: $a")
+    println("Sting to integer value: $b")
+    println("String to double: $c")
+}
